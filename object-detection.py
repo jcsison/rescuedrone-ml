f7d7376467d99f5c4e923ebeec823373e0ec3a8b
jcsison <jesser.sison@live.com>
2018-02-10 17:47:37 -0800

    Initial commit

---
+++ object-detection.py
@@ -0,0 +1,100 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+import cv2
+cap = cv2.VideoCapture(0)
+
+sys.path.append("..")
+
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+if not os.path.isfile(PATH_TO_CKPT):
+    opener = urllib.request.URLopener()
+    opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+    tar_file = tarfile.open(MODEL_FILE)
+    for file in tar_file.getmembers():
+        file_name = os.path.basename(file.name)
+        if 'frozen_inference_graph.pb' in file_name:
+            tar_file.extract(file, os.getcwd())
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+    try:
+        while True:
+            ret, image_np = cap.read()
+            if not ret:
+                cap.release()
+                print("Released video source.")
+                break
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+            (boxes, scores, classes, num) = sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
+
+            # Testing class detection
+            for i in range(min(20, np.squeeze(boxes).shape[0])):
+                if np.squeeze(classes).astype(np.int32)[i] in category_index.keys() and np.squeeze(scores)[i] > 0.5:
+                  class_name = category_index[np.squeeze(classes).astype(np.int32)[i]]['name']
+                  score_value = np.squeeze(scores)[i]
+                  print("{0} - {1:.2f}".format(class_name, score_value))
+
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                image_np,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                min_score_thresh=.075,
+                agnostic_mode=True,
+                line_thickness=4)
+            # image_np = cv2.resize(image_np, (1200, 900))
+            # image_np = cv2.resize(image_np, (800, 600))
+
+            cv2.imshow('Object Detection', image_np)
+            if cv2.waitKey(50) & 0xFF == ord('q'):
+                cv2.destroyAllWindows()
+                break
+    except KeyboardInterrupt:
+        cap.release()
+        print("Released video source.")
