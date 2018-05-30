285b0f07499ceeed4e90135c5c8ec2b764dfdcd2
jcsison <jesser.sison@live.com>
2018-05-29 19:57:18 -0700

    [05/29/18 19:57] Angle detection rewrite, code clean-up

--- object_detection_test.py
+++ object_detection_test.py
@@ -44,6 +44,7 @@ class FileVideoStream:
         return self
 
     def update(self):
+        global counter
         counter = 0
         while True:
             counter += 1
@@ -54,7 +55,8 @@ class FileVideoStream:
                 if not grabbed:
                     self.stop()
                     return
-                if counter % 20 == 0:
+                if counter % 1 == 0:
+                    frame = imutils.resize(frame, width=cap_width//3)
                     self.Q.put(frame)
 
     def read(self):
@@ -69,19 +71,16 @@ class FileVideoStream:
     def get_stream(self):
         return self.stream
 
-def detect(video_input=0, show=False, write=False, threshold=0.5, process=False):
+def detection(video_input=0, show=False, write=False, threshold=0.5):
     def angle(coord):
-        x, y = coord[0], coord[1]
-        if x != 0:
-            theta = np.rad2deg(abs(np.arctan(y / x)))
-        else:
-            theta = 90
-        if x < 0 and y < 0:
-            theta = theta + 90
-        elif x < 0 and y >= 0:
-            theta = theta + 180
-        elif x >= 0 and y >= 0:
-            theta = theta + 270
+        def unit_vector(vector):
+            return vector / np.linalg.norm(vector)
+
+        v1_u = unit_vector((1, 0))
+        v2_u = unit_vector(coord)
+        theta = np.rad2deg(np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)))
+        if (coord[1] > 0):
+            theta = 360 - theta
         return theta
 
     def direction(theta):
@@ -103,23 +102,15 @@ def detect(video_input=0, show=False, write=False, threshold=0.5, process=False)
             direction = 'SE'
         return direction
 
-    def next_seed():
-        blur = round(1.5*random.random())*2 + 1
-        tilt = round(360*random.random())
-        erode = round(1*random.random()) + 1
-        dilate = round(1*random.random()) + 1
-        return blur, tilt, erode, dilate
-
     if (video_input == '0'):
         fvs = FileVideoStream(0).start()
     else:
         fvs = FileVideoStream(video_input).start()
 
     cap = fvs.get_stream()
+    global cap_width
     cap_width = int(cap.get(3))
     cap_height = int(cap.get(4))
-
-    ref = (0, 0, 0, 0)
 
     if write:
         out = cv2.VideoWriter('output/output_{:%y%m%d_%H%M%S}.avi'.format(
@@ -157,92 +148,84 @@ def detect(video_input=0, show=False, write=False, threshold=0.5, process=False)
     category_index = label_map_util.create_category_index(categories)
 
     with detection_graph.as_default():
-      with tf.Session(graph=detection_graph) as sess:
-        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        with tf.Session(graph=detection_graph) as sess:
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-        try:
-            init = False
-            count = 0
-            center_curr = np.array([0, 0])
-            center_delta_curr = np.array([0, 0])
-            center_ref = np.array([0.5, 0.5])
-            theta = 0
+            try:
+                init = False
+                count = 0
+                theta = 0
+                center_curr = np.array([0, 0])
+                center_delta_curr = np.array([0, 0])
+                center_ref = np.array([0.5, 0.5])
+                center_prev = np.array([0.5, 0.5])
+                ref = (0, 0, 0, 0)
 
-            while True:
-                image_np = fvs.read()
-                image_np = imutils.resize(image_np, width=cap_width//3)
+                while True:
+                    image_np = fvs.read()
 
-                if not fvs.more():
-                    fvs.stop()
-                    print("Released video source.")
-                    break
+                    if not fvs.more():
+                        fvs.stop()
+                        print("Released video source.")
+                        break
 
-                if process:
-                    blur, tilt, erode, dilate = next_seed()
-                    img_ycrcb = cv2.cvtColor(image_np, cv2.COLOR_BGR2YCR_CB)
-                    channels = cv2.split(img_ycrcb)
-                    channels[0] = cv2.equalizeHist(channels[0])
-                    img_ycrcb = cv2.merge(channels)
-                    image_np = cv2.cvtColor(img_ycrcb, cv2.COLOR_YCR_CB2BGR)
-                    image_np = cv2.GaussianBlur(image_np, (blur, blur), 0)
-                    # (h, w) = image_np.shape[:2]
-                    # image_np = cv2.copyMakeBorder(image_np, 500, 500, 500, 500, cv2.BORDER_WRAP)
-                    # (h2, w2) = image_np.shape[:2]
-                    # image_np = cv2.warpAffine(image_np, cv2.getRotationMatrix2D((w2 / 2, h2 / 2), tilt, 1.0), (w2, h2))
-                    # image_np = image_np[int(h2//2 - h//2):int(h2//2 + h/2), int(w2//2 - w/2):int(w2//2 + w/2)]
-                    # image_np = cv2.erode(image_np, np.ones((erode, erode)))
-                    # image_np = cv2.dilate(image_np, np.ones((dilate, dilate)))
+                    if (counter % 20 == 0):
+                        image_np_expanded = np.expand_dims(image_np, axis=0)
+                        (boxes, scores, classes, num) = sess.run([detection_boxes,
+                            detection_scores, detection_classes, num_detections],
+                            feed_dict={image_tensor: image_np_expanded})
 
-                image_np_expanded = np.expand_dims(image_np, axis=0)
-                (boxes, scores, classes, num) = sess.run([detection_boxes,
-                    detection_scores, detection_classes, num_detections],
-                    feed_dict={image_tensor: image_np_expanded})
+                        for i in range(min(20, np.squeeze(boxes).shape[0])):
+                            class_value = np.squeeze(classes).astype(np.int32)[i]
+                            score_value = np.squeeze(scores)[i]
+                            if class_value in category_index.keys():
+                                ymin, xmin, ymax, xmax = tuple(np.squeeze(boxes)[i].tolist())
+                                class_name = category_index[class_value]['name']
+                                if score_value > threshold:
+                                    center = np.array([(xmin + xmax) / 2, (ymin + ymax) / 2])
+                                    center_curr = center
+                                    center_delta_curr = center_curr - center_prev
+                                    distance = np.linalg.norm(center_ref - center_curr)
+                                    if distance > 0.25 and count < 5 and init:
+                                        count += 1
+                                        scores[0][i] = 0
+                                    else:
+                                        count = 0
+                                        if distance > 0.1 or not init:
+                                            init = True
+                                            center_prev = center_ref
+                                            center_ref = center_curr
+                                            ref = (xmin, xmax, ymin, ymax)
+                                            theta = angle(center_ref - center_prev)
+                                        print(('{0} @ {1:.5}% | angle: {2:.6} | direction: {3:2} | center: ({4[0]:.2f}, {4[1]:.2f}) | distance from ref: {5:.6}').format(class_name, str(score_value * 100), str(theta), direction(theta), center, str(distance)))
 
-                for i in range(min(20, np.squeeze(boxes).shape[0])):
-                    class_value = np.squeeze(classes).astype(np.int32)[i]
-                    score_value = np.squeeze(scores)[i]
-                    if class_value in category_index.keys():
-                        ymin, xmin, ymax, xmax = tuple(np.squeeze(boxes)[i].tolist())
-                        class_name = category_index[class_value]['name']
-                        if score_value > threshold:
-                            center = np.array([(xmin + xmax) / 2, (ymin + ymax) / 2])
-                            center_prev = center_curr
-                            center_curr = center
-                            center_delta_prev = center_delta_curr
-                            center_delta_curr = center_curr - center_prev
-                            distance = np.linalg.norm(center_ref - center)
-                            if distance > 0.25 and count < 5 and init:
-                                count += 1
-                                scores[0][i] = 0
-                            else:
-                                count = 0
-                                if distance > 0.0625 or not init:
-                                    init = True
-                                    center_ref = center_curr
-                                    ref = (xmin, xmax, ymin, ymax)
-                                    theta = angle(center_delta_curr)
-                                print(('{0} @ {1:.5}% | angle: {2:.6} | direction: {3:2} | center: ({4[0]:.2f}, {4[1]:.2f}) | distance from ref: {5:.6}').format(class_name, str(score_value * 100), str(theta), direction(theta), center, str(distance)))
+                    coord_prev = (int(center_prev[0] * cap_width // 3), int(center_prev[1] * cap_height // 3))
+                    coord_ref = (int(center_ref[0] * cap_width // 3), int(center_ref[1] * cap_height // 3))
+                    min_vertex = (int(ref[0] * cap_width // 3), int(ref[2] * cap_height // 3))
+                    max_vertex = (int(ref[1] * cap_width // 3), int(ref[3] * cap_height // 3))
+                    cv2.rectangle(image_np, min_vertex, max_vertex, (255, 0, 0), 2)
+                    cv2.arrowedLine(image_np, coord_prev, coord_ref, (255, 0, 0), 3, tipLength=0.25)
+                    display_str = 'angle: {0:.2f} ({1})'.format(theta, direction(theta))
+                    cv2.putText(image_np, display_str, (int(ref[0] * cap_width // 3), int((ref[2] - 0.0275) * cap_height // 3)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 0, 0), 2, cv2.LINE_AA)
 
-                cv2.rectangle(image_np, (int(ref[0] * cap_width // 3), int(ref[2] * cap_height // 3)), (int(ref[1] * cap_width // 3), int(ref[3] * cap_height // 3)), (255, 0, 0), 2)
+                    if write:
+                        out.write(image_np)
 
+                    if show:
+                        cv2.imshow('Object Detection', image_np)
+
+                    if cv2.waitKey(50) & 0xFF == ord('q'):
+                        cv2.destroyAllWindows()
+                        break
+            except KeyboardInterrupt:
+                fvs.stop()
                 if write:
-                    out.write(image_np)
-
-                if show:
-                    cv2.imshow('Object Detection', image_np)
-
-                if cv2.waitKey(50) & 0xFF == ord('q'):
-                    cv2.destroyAllWindows()
-                    break
-        except KeyboardInterrupt:
-            fvs.stop()
-            if write:
-                out.release()
-            print("Released video source.")
+                    out.release()
+                print("Released video source.")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(usage='python3 object_detection_test.py [-f file_path] [-s] [-w] [-t threshold]')
@@ -250,7 +233,6 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--show', action='store_const', const=True, default=False, help='Show video output')
     parser.add_argument('-w', '--write', action='store_const', const=True, default=False, help='Write video output to file')
     parser.add_argument('-t', '--threshold', type=float, default=0.5, help='Threshold for detection')
-    parser.add_argument('-p', '--process', action='store_const', const=True, default=False, help='Process video input')
     args = parser.parse_args()
 
-    detect(video_input=args.file, show=args.show, write=args.write, threshold=args.threshold, process=args.process)
+    detection(video_input=args.file, show=args.show, write=args.write, threshold=args.threshold)
